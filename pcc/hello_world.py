b98d5f14d154241c241244561a4accf2a9af25b1
B B <blackburnbriand@gmail.com>
2022-05-21 22:27:35 -0700

    upload two exercise files

---
+++ pcc/hello_world.py
@@ -0,0 +1,12 @@
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+message = "I'm using a halfquote."
+print(message)
+
+message = 'I am using a "fullquote"'
+print(message)
+
---
+++ pcc/name.py
@@ -0,0 +1,4 @@
+name = "neil gaiman"
+print(name.title())
+print(name.upper())
+print(name.lower())
