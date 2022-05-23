3933c5d9339c73229395f4b587445b4209e994c9
B B <blackburnbriand@gmail.com>
2022-05-22 18:30:18 -0700

    update bookmark complete more exercises

---
+++ pcc/cars.py
@@ -0,0 +1,17 @@
+cars = ['bmw','audi','toyota','subaru',]
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+cars = ['bmw','audi','toyota','subaru',]
+print("Original list:")
+print(cars)
+print("\nSorted list:")
+print(sorted(cars))
+print("\nOriginal list, again:")
+print(cars)
+print("\nReverse order:")
+cars.reverse()
+print(cars)
+print(len(cars))
